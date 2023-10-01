6f90917cc8c830c90d7ac8eb151913a5e6a1628b
Andrew Tridgell <andrew@tridgell.net>
2023-10-01 17:18:48 +1100

    autotest: added PlaneJSON

--- Tools/autotest/pysim/vehicleinfo.py
+++ Tools/autotest/pysim/vehicleinfo.py
@@ -298,6 +298,10 @@ class VehicleInfo(object):
                 "waf_target": "bin/arduplane",
                 "default_params_filename": [], # defaults are loaded in SIM_Plane.cpp
             },
+            "planeJSON": {
+                "waf_target": "bin/arduplane",
+                "default_params_filename": "default_params/plane.parm",
+            },
             "quadplane-copter_tailsitter": {
                 "waf_target": "bin/arduplane",
                 "default_params_filename": ["default_params/quadplane.parm","default_params/quadplane-copter_tailsitter.parm"],
